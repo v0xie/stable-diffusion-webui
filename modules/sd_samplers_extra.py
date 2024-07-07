ff93d186358c43420cd26278318a955eb364ac04
v0xie <28695009+v0xie@users.noreply.github.com>
2024-07-07 14:09:13 -0700

    add dpm++ 2m cfg++

--- modules/sd_samplers_extra.py
+++ modules/sd_samplers_extra.py
@@ -79,6 +79,9 @@ def sample_dpmpp_2m_cfgpp(model, x, sigmas, extra_args=None, callback=None, disa
     """DPM-Solver++(2M) CFG++. 
     Modified from https://github.com/crowsonkb/k-diffusion/blob/master/k_diffusion/sampling.py
     """
+    model.cond_scale_miltiplier = 1 / 12.5
+    model.need_last_noise_uncond = True
+
     extra_args = {} if extra_args is None else extra_args
     s_in = x.new_ones([x.shape[0]])
     sigma_fn = lambda t: t.neg().exp()
